07275159c3f769ca6f55a7b76ca96d2b9049982d
arvaianett <arvaianett@gmail.com>
2017-11-27 17:34:07 +0100

    feat(w2-day-01):add exercise13

---
+++ week-02/day-01/exercise13/exercise13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            int oneDayHours = 24;
+            int oneDayMinutes = 60;
+            int oneDaySeconds = 60;
+            
+            Console.WriteLine(oneDaySeconds * oneDayMinutes * oneDayHours - currentSeconds * currentMinutes * currentHours);
+
+            Console.ReadLine();
+        }
+    }
+}
