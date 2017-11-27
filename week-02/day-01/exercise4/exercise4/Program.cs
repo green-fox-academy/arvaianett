f8d92fea92fa8d51b6e3173fe8b63ebfa85c8772
arvaianett <arvaianett@gmail.com>
2017-11-27 15:52:15 +0100

    feat(w2-day-01):add exercise4

---
+++ week-02/day-01/exercise4/exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anett Arvai");
+            Console.WriteLine("24");
+            Console.WriteLine("1.65");
+
+            Console.ReadLine();
+        }
+    }
+}
