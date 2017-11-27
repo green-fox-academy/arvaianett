6de4faf7d18d139695d6ecf6bba67386f2b14f3b
arvaianett <arvaianett@gmail.com>
2017-11-27 16:16:50 +0100

    feat(w2-day-01):add exercise5

---
+++ week-02/day-01/exercise5/exercise5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number0 = 22;
+            int number1 = 13;
+
+            Console.WriteLine(number0 + number1);
+            Console.WriteLine(number0 - number1);
+            Console.WriteLine(number0 * number1);
+            Console.WriteLine(number0 / 13.0);
+            Console.WriteLine(number0 / number1);
+            Console.WriteLine(number0 % number1);
+
+            Console.ReadLine();
+
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+
+            // Print the integer part of 22 divided by 13
+
+            // Print the reminder of 22 divided by 13
+        }
+    }
+}
