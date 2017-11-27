2c57c642cd5d44f16cfc5d59b7e214c83f15e697
arvaianett <arvaianett@gmail.com>
2017-11-27 16:52:43 +0100

    feat(w2-day-01):add exercise10

---
+++ week-02/day-01/exercise10/exercise10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Anett";
+            int age = 24;
+            double height = 165;
+            bool married = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+
+            Console.ReadLine();
+        }
+    }
+}
