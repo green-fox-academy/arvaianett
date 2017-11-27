83333dfb124cb3ea6455013d1ed44c072bca3dba
arvaianett <arvaianett@gmail.com>
2017-11-27 16:38:49 +0100

    feat(w2-day-01):add exercise6

---
+++ week-02/day-01/exercise6/exercise6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = 6;
+            int week = 17;
+            int work = 52;
+
+            Console.WriteLine(week * 5 * hours);
+            Console.WriteLine(510 / (17 * 52.0));
+            
+            Console.ReadLine();
+        }
+    }
+}
