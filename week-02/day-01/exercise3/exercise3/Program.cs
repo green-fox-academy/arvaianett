523d926324de76e6e4b53cc447befd947289830b
arvaianett <arvaianett@gmail.com>
2017-11-27 15:43:36 +0100

    feat(w2-day-01):add exercise3

---
+++ week-02/day-01/exercise3/exercise3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Balint!");
+            Console.WriteLine("Hello Szabolcs!");
+            Console.WriteLine("Hello Nyina!");
+
+            Console.ReadLine();
+        }
+    }
+}
