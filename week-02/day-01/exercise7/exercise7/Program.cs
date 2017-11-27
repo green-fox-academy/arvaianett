51dbb6a2018284d4c2fe4a5b035df24a874f6f2b
arvaianett <arvaianett@gmail.com>
2017-11-27 16:43:01 +0100

    feat(w2-day-01):add exercise7

---
+++ week-02/day-01/exercise7/exercise7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 8;
+
+            Console.WriteLine("My favorite number is : " + number);
+
+            Console.ReadLine();
+        }
+    }
+}
