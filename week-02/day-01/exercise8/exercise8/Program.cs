30f3e2e86cac75bef9bf924eb0f8a5935676ff39
arvaianett <arvaianett@gmail.com>
2017-11-27 16:46:36 +0100

    feat(w2-day-01):add exercise8

---
+++ week-02/day-01/exercise8/exercise8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 526;
+            int b = 123;
+        }
+    }
+}
