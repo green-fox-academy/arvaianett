d8656613af6bc2a5c6ec15222d45d18b83b5a9d3
arvaianett <arvaianett@gmail.com>
2017-11-28 16:39:09 +0100

    feat(w2-day-02): add d1 exercise28

---
+++ week-02/day-02/d1-exercise28/d1-exercise28/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace d1_exercise28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number: ");
+            string number = Console.ReadLine();
+
+            if (int.Parse(number) == int.Parse(number))
+            {
+                Console.WriteLine("*");
+            }
+            if (int.Parse(number) > 0 || int.Parse(number) <= 0)
+            {
+                Console.WriteLine("**");
+            }
+            if (int.Parse(number) == int.Parse(number) || int.Parse(number) != int.Parse(number))
+            {
+                Console.WriteLine("***");
+            }
+            if (int.Parse(number) == 10 || int.Parse(number) != 10)
+            {
+                Console.WriteLine("****");
+            }
+            Console.ReadLine();
+        }
+    }
+}
