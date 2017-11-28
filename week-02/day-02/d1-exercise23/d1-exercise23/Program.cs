096a8edc0b9d102a3a27e10b621c85d8be0dfbec
arvaianett <arvaianett@gmail.com>
2017-11-28 13:58:44 +0100

    feat(w2-day-02): add d1 exercise23

---
+++ week-02/day-02/d1-exercise23/d1-exercise23/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace d1_exercise23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while (a < 100)
+            {
+                Console.WriteLine("I wont't cheat on the exam!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
