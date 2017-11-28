dd1ff33c1d0a60597917549e04950a099e737591
arvaianett <arvaianett@gmail.com>
2017-11-28 11:34:36 +0100

    feat(w2-day-02): add d1 exercise15

---
+++ week-02/day-02/d1-exercise15/d1-exercise15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace d1_exercise15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kilometer = Console.ReadLine();
+
+            Console.WriteLine("Distance in kilometers: " + kilometer + " km");
+
+            double miles = double.Parse(kilometer) * 0.621371;
+
+            Console.WriteLine("Distance: " + miles + " mi");
+
+            Console.ReadLine();
+        }
+    }
+}
