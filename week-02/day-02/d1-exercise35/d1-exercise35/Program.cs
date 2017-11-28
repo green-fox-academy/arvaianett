b41bb3ecf24e77663d00baa90277067f7c8b1c8f
arvaianett <arvaianett@gmail.com>
2017-11-28 22:00:51 +0100

    feat(w2-day-02): add d1 exercise35

---
+++ week-02/day-02/d1-exercise35/d1-exercise35/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace d1_exercise35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(" % % % %  ");
+                }
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine("  % % % % ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
