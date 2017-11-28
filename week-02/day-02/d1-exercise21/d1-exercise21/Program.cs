df843e370477c9f875ef85fae8da8c9e50a68323
arvaianett <arvaianett@gmail.com>
2017-11-28 13:48:32 +0100

    feat(w2-day-02): add d1 exercise21

---
+++ week-02/day-02/d1-exercise21/d1-exercise21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace d1_exercise21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of girls: ");
+            string girls = Console.ReadLine();
+
+            Console.WriteLine("Number of boys: ");
+            string boys = Console.ReadLine();
+            
+            if (int.Parse(girls) == int.Parse(boys) && int.Parse(girls) + int.Parse(boys) > 20)
+            {
+                Console.WriteLine("The party is exellent!");
+            }
+            else if (int.Parse(girls) == 0)
+            {
+                Console.WriteLine("Sausage party");
+            }
+            else if (int.Parse(girls) + int.Parse(boys) > 20 && int.Parse(girls) != int.Parse(boys))
+            {
+                Console.WriteLine("Quite cool party!");
+            }
+            else if (int.Parse(girls) + int.Parse(boys) < 20)
+            {
+                Console.WriteLine("Average party...");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
