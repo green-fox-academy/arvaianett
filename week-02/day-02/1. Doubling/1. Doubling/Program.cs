1dc121e9aa9d258b8292e737b69814e9b68d4bc3
arvaianett <arvaianett@gmail.com>
2017-11-28 22:19:56 +0100

    feat(w2-day-02): add 1. Doubling

---
+++ week-02/day-02/1. Doubling/1. Doubling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1._Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doubling();
+            Console.ReadLine();
+        }
+
+        public static void Doubling()
+        {
+            int ak = 123;
+            Console.WriteLine(ak * ak);
+        }
+    }
+}
