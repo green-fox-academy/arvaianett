107d8a7775233ac51661f84fca53af8ec5eae2ab
arvaianett <arvaianett@gmail.com>
2017-11-28 15:55:50 +0100

    feat(w2-day-02): add d1 exercise26

---
+++ week-02/day-02/d1-exercise26/d1-exercise26/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace d1_exercise26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first number: ");
+            string first = Console.ReadLine();
+
+            Console.WriteLine("second number: ");
+            string second = Console.ReadLine();
+
+            if (int.Parse(second) < int.Parse(first))
+            {
+                Console.WriteLine("The second number should be bigger");
+            }
+
+            for (int i = int.Parse(first); i < int.Parse(second); i++)
+            {
+                if (int.Parse(second) > int.Parse(first))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
