444263c2ca81610fc762ac0948eb4a87be996650
arvaianett <arvaianett@gmail.com>
2017-11-28 11:05:02 +0100

    feat(w2-day-02): add day-01 exercise14

---
+++ week-02/day-02/d1-exercise14/d1-exercise14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace d1_exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello user what is your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name + "!");
+
+            Console.ReadLine();
+        }
+    }
+}
