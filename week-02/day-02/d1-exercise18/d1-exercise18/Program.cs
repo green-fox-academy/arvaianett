8e8f876b34b42b18fa929c874612af27f1143635
arvaianett <arvaianett@gmail.com>
2017-11-28 12:04:57 +0100

    feat(w2-day-02): add d1 exercise18

---
+++ week-02/day-02/d1-exercise18/d1-exercise18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace d1_exercise18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringnumber = Console.ReadLine();
+
+            if (double.Parse(stringnumber) % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else if (double.Parse(stringnumber) % 2 != 0)
+            {
+                Console.WriteLine("Odd");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
