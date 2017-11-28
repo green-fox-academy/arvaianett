e48cfed18c788078232165e24961a7a8f38b41a9
arvaianett <arvaianett@gmail.com>
2017-11-28 12:19:22 +0100

    feat(w2-day-02): add d1 exercise19

---
+++ week-02/day-02/d1-exercise19/d1-exercise19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace d1_exercise19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            if (double.Parse(number) <= 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+            else if (double.Parse(number) == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (double.Parse(number) == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else if (double.Parse(number) > 2)
+            {
+                Console.WriteLine("A lot");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
