938c324d8b2ac4949a8f189a593a2c8ead147a35
arvaianett <arvaianett@gmail.com>
2017-11-28 15:30:57 +0100

    feat(w2-day-02): add d1 exercise25

---
+++ week-02/day-02/d1-exercise25/d1-exercise25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace d1_exercise25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            
+            for(int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i + " * " + number + " = " + (i * int.Parse(number)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
