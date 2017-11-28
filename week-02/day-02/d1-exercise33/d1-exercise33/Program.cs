49ae1fd95aede51025bc219c79444ddfd24bd4ea
arvaianett <arvaianett@gmail.com>
2017-11-28 21:43:29 +0100

    feat(w2-day-02): add d1 exercise33

---
+++ week-02/day-02/d1-exercise33/d1-exercise33/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace d1_exercise33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool num = true;
+
+            do
+            {
+                Console.WriteLine("Have a guess: ");
+                string number = Console.ReadLine();
+
+                if (int.Parse(number) > 8)
+                {
+                    Console.WriteLine("The stored number is higher");
+                }
+                else if (int.Parse(number) < 8)
+                {
+                    Console.WriteLine("The stored number is lower");
+                }
+                else if (int.Parse(number) == 8)
+                {
+                    Console.WriteLine("You found the number: 8");
+                    num = false;
+                }
+            } while (num);
+
+            Console.ReadLine();
+        }
+    }
+}
