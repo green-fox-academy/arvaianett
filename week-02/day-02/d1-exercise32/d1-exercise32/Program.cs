da05a7b5effdc7c89ab5cb7a7089b1435a8f7694
arvaianett <arvaianett@gmail.com>
2017-11-28 21:11:30 +0100

    feat(w2-day-02): add d1 exercise32

---
+++ week-02/day-02/d1-exercise32/d1-exercise32/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace d1_exercise32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type 0: ");
+            string number = Console.ReadLine();
+
+            for (int j = int.Parse(number); j < 5; j++)
+
+                for (int i = int.Parse(number); i < 5; i++)
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("%%%%%");
+                    }
+                    if (i == 0)
+                    {
+                        Console.WriteLine("%%  %");
+                    }
+                    if (i == 0)
+                    {
+                        Console.WriteLine("% % %");
+                    }
+                    if (i == 0)
+                    {
+                        Console.WriteLine("%  %%");
+                    }
+                    if (i == 0)
+                    {
+                        Console.WriteLine("%%%%%");
+                    }
+
+                    Console.ReadLine();
+                }
+
+            
+        }
+    }
+}
