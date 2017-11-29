0b2c88d1108f4bd9a0aa63a41c985d8e12aa640c
arvaianett <arvaianett@gmail.com>
2017-11-29 11:52:27 +0100

    feat(w2-day-03): add d2 2. Greeter function

---
+++ week-02/day-03/2. Greeter function/2. Greeter function/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2._Greeter_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet();
+            Console.ReadLine();
+        }
+
+        static void Greet()
+        {
+            string al = "Greenfox";
+            Console.WriteLine("Greetings dear, " + al);
+        }
+    }
+}
