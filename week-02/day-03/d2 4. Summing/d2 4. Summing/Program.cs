92e0277e8cec1d2b1d87e053a34e14bb7b3b8c96
arvaianett <arvaianett@gmail.com>
2017-11-29 14:47:57 +0100

    feat(w2-day-03): add d2 4. Summing

---
+++ week-02/day-03/d2 4. Summing/d2 4. Summing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace d2_4._Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double parameter = 10;
+
+            Sum(parameter);
+            Console.ReadLine();
+        }
+        static void Sum(double par)
+        {
+            double number = 0;
+            for (int i = 1; i <= par; i++)
+            {
+                number += i;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
