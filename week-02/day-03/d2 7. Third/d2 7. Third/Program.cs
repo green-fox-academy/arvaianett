3f1a669112c34009b49ee41a7af77fa7a738ad7f
arvaianett <arvaianett@gmail.com>
2017-11-29 15:19:34 +0100

    feat(w2-day-03): add d2 7. Third

---
+++ week-02/day-03/d2 7. Third/d2 7. Third/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace d2_7._Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] q = new int[4];
+            q[0] = 4;
+            q[1] = 5;
+            q[2] = 6;
+            q[3] = 7;
+
+            Console.WriteLine(q[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
