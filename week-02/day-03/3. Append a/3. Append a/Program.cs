96d82fa7caae6960bdea1da6bea84c41fdd28b7e
arvaianett <arvaianett@gmail.com>
2017-11-29 12:52:32 +0100

    feat(w2-day-03): add d2 3.AppendA

---
+++ week-02/day-03/3. Append a/3. Append a/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3._Append_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+
+            AppendA(am);
+            Console.ReadLine();
+        }
+
+        static void AppendA(string something)
+        {
+            something = something + "a";
+
+            Console.WriteLine(something);
+        }
+    }
+}
