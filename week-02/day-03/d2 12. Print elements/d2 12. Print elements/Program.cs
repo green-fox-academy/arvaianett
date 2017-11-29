cbb90e68daa7f4d60ef3bdadc722ed9f73c3de5e
arvaianett <arvaianett@gmail.com>
2017-11-29 17:08:07 +0100

    feat(w2-day-03): add d3 12. Print elements

---
+++ week-02/day-03/d2 12. Print elements/d2 12. Print elements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace d2_12._Print_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+            
+            foreach (int number in af)
+            {
+                Console.WriteLine(number);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
