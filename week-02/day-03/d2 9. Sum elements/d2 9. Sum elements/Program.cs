f19a7b1cf6e5ccb6cde8939173de659ed1b97b96
arvaianett <arvaianett@gmail.com>
2017-11-29 15:58:46 +0100

    feat(w2-day-03): add d2 9. Sum elements

---
+++ week-02/day-03/d2 9. Sum elements/d2 9. Sum elements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace d2_9._Sum_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] r = new int[4];
+            r[0] = 54;
+            r[1] = 23;
+            r[2] = 66;
+            r[3] = 12;
+
+            Console.WriteLine(r[1] + r[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
