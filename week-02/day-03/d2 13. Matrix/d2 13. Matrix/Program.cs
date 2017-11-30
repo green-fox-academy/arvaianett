e38a74bdc31d2aff9d3bb44b62fecd6d88ac9419
arvaianett <arvaianett@gmail.com>
2017-11-30 11:14:15 +0100

    feat(w2-day-04): add d2 13. Matrix

---
+++ week-02/day-03/d2 13. Matrix/d2 13. Matrix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace d2_13._Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4, 4] { { 1, 0, 0, 0}, { 0, 1, 0, 0}, { 0, 0, 1, 0}, { 0, 0, 0, 1} };
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(array [i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
