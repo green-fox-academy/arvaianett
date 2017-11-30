9c5db3e83bb544c919c90aadaaeecde04070ccf0
arvaianett <arvaianett@gmail.com>
2017-11-30 17:10:56 +0100

    feat(w2-day-04): add d2 5. Factorial

---
+++ week-02/day-04/d2-5.Factorial/d2-5.Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace d2_5.Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorio(number));
+            
+            Console.ReadLine();
+        }
+
+        static int Factorio(int factorial)
+        {
+            int numb = 1;
+
+            for (int i = 1; i <= factorial; i++)
+            {
+                numb *= i;
+            }
+
+            return numb;
+        }
+    }
+}
