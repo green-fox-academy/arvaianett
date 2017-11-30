1e654ed4092f6c83b97248aec5b9dbd361609580
arvaianett <arvaianett@gmail.com>
2017-11-30 12:16:16 +0100

    feat(w2-day-04): add d2 16. Append a

---
+++ week-02/day-04/d2 16. Append a/d2 16. Append a/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace d2_16._Append_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nimals = { "kuty", "macsk", "cic" };
+            Letter(nimals);
+            Console.ReadLine();
+        }
+
+        static void Letter(string[] animals)
+        {
+            for (int i = 0; i < animals.Length; i++)
+            {
+                animals[i] += "a";
+                Console.WriteLine(animals[i]);
+            }
+        }
+    }
+}
