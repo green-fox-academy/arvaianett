34115da3c64fe532dfb09a76fd7665519cacf468
arvaianett <arvaianett@gmail.com>
2017-11-30 13:29:31 +0100

    feat(w2-day-04): add d2 18. Sum all elements

---
+++ week-02/day-04/18. Sum all elements/18. Sum all elements/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _18._Sum_all_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = { 3, 4, 5, 6, 7 };
+
+            int sum = ai[0] + ai[1] + ai[2] + ai[3] + ai[4];
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
