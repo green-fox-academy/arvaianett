aa12bb981e73615c6691e1761e4cf60eddfafad5
arvaianett <arvaianett@gmail.com>
2017-11-30 13:50:07 +0100

    feat(w2-day-04): d2 20. Reverse list

---
+++ week-02/day-04/d2 20. Reverse list/d2 20. Reverse list/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace d2_20._Reverse_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = { 3, 4, 5, 6, 7 };
+
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(aj[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
