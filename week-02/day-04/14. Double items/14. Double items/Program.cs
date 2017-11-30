8bf7a2e44c6b73c7d2aea266859ec6dbc9a01c37
arvaianett <arvaianett@gmail.com>
2017-11-30 11:32:17 +0100

    feat(w2-day-04): add d2 14. Double items

---
+++ week-02/day-04/14. Double items/14. Double items/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _14._Double_items
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ag = { 3, 4, 5, 6, 7 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(ag[i] * 2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
