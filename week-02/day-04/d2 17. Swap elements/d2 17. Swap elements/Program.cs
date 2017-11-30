2e2d05d65d4ba8260d1dac0e962856eb0846b3bb
arvaianett <arvaianett@gmail.com>
2017-11-30 13:25:23 +0100

    feat(w2-day-04): add d2 17. Swap elements

---
+++ week-02/day-04/d2 17. Swap elements/d2 17. Swap elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace d2_17._Swap_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+
+            string temp1 = abc[0];
+            string temp2 = abc[2];
+
+            abc[0] = temp2;
+            abc[2] = temp1;
+
+            foreach (string ab in abc)
+            {
+                Console.WriteLine(ab);
+            }
+            Console.ReadLine();
+        }
+    }
+}
