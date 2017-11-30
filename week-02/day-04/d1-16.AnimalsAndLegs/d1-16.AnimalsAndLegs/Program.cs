fb68572ae9d5aefe687deb93357af0d9a00e5244
arvaianett <arvaianett@gmail.com>
2017-11-30 17:30:37 +0100

    feat(w2-day-04): add d1 16. AnimalsAndLegs

---
+++ week-02/day-04/d1-16.AnimalsAndLegs/d1-16.AnimalsAndLegs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace d1_16.AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of chickens: ");
+            int chickens = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Number of pigs: ");
+            int pigs = int.Parse(Console.ReadLine());
+
+            int legs1 = 2;
+            int legs2 = 4;
+
+            int all = chickens * legs1 + pigs * legs2;
+            
+            Console.WriteLine("They have " + all + " legs.");
+
+            Console.ReadLine();
+        }
+    }
+}
