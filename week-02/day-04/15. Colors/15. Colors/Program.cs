f8c79f6e61317df085a7d041c20225ed21b700e1
arvaianett <arvaianett@gmail.com>
2017-11-30 11:49:02 +0100

    feat(w2-day-04): add d2 15. Colors

---
+++ week-02/day-04/15. Colors/15. Colors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _15._Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] colors = new string[3, 5] {{ "lime", "forest green", "olive", "pale green", "spring green" },
+                                                { "orange red", "red", "tomato", "", ""}, 
+                                                { "orchid", "violet", "pink", "hot pink", ""} };
+            Console.ReadLine();
+        }
+    }
+}
