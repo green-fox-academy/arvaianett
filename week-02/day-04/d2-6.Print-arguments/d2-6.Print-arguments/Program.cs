1e51aeb5c896cbee5b645af3733094ff13892232
arvaianett <arvaianett@gmail.com>
2017-11-30 16:20:47 +0100

    feat(w2-day-04): add 6. Print arguments

---
+++ week-02/day-04/d2-6.Print-arguments/d2-6.Print-arguments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace d2_6.Print_arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer("first");
+            Printer("first", "second");
+            Printer("first", "second", "third", "fourh");
+            
+            Console.ReadLine();
+        }
+
+        static void Printer(params string[] print)
+        {
+            foreach (string p in print)
+            {
+                Console.Write(p);
+            }
+            Console.WriteLine();
+        }
+    }
+}
