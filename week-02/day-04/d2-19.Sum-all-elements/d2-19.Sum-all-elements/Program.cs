b5a7d73881b31c7f9addbd16ef36f2f728006974
arvaianett <arvaianett@gmail.com>
2017-11-30 14:09:40 +0100

    feat(w2-day-04): d2 19. Sum all elements

---
+++ week-02/day-04/d2-19.Sum-all-elements/d2-19.Sum-all-elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace d2_19.Sum_all_elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = { 3, 4, 5, 6, 7 };
+
+            int sum = ai[0]+ ai[1] + ai[2] + ai[3] + ai[4];
+
+            Console.WriteLine(sum);
+
+            int summary = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                summary += ai[i];
+            }
+            Console.WriteLine(summary);
+            Console.ReadLine();
+        }
+    }
+}
