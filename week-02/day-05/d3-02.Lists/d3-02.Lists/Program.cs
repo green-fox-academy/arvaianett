ed9c07d9aeb07d01da42038c0ea97615186ac305
arvaianett <arvaianett@gmail.com>
2017-12-01 12:19:05 +0100

    feat(w2-day-05): add d3 02. Lists

---
+++ week-02/day-05/d3-02.Lists/d3-02.Lists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d3_02.Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string> { girls[0], boys[0], girls[1], boys[1], girls[2], boys[2],
+                                           girls[3], boys[3], girls[4], boys[4], boys[5] };
+
+            foreach (var o in order)
+            {
+                Console.WriteLine(o);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
