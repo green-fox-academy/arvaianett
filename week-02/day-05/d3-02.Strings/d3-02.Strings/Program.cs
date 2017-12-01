09762d3a8a2e383238cf386f96d8e1fa6b5400d9
arvaianett <arvaianett@gmail.com>
2017-12-01 13:14:58 +0100

    feat(w2-day-05): add d3-03.Lists

---
+++ week-02/day-05/d3-02.Strings/d3-02.Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace d3_02.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ week-02/day-05/d3-03.Lists/d3-03.Lists/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d3_03.Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(far[i] + "a"); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
