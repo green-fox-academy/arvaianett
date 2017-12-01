d5d7a75295bb9241bd5bee089eedfed8b6f9892a
arvaianett <arvaianett@gmail.com>
2017-12-01 11:28:59 +0100

    feat(w2-day-05): add 01. Strings

---
+++ week-02/day-05/d3-01-Strings/d3-01-Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace d3_01_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+
+            string correct = example.Replace("dishwasher", "galaxy");
+
+            Console.WriteLine(correct);
+
+            Console.ReadLine();
+        }
+    }
+}
