8627b7684375ec13ec8e6e651392474c5586acea
arvaianett <arvaianett@gmail.com>
2017-12-01 14:42:16 +0100

    feat(w2-day-05): add d3 06. Lists

---
+++ week-02/day-05/d3-06.Lists/d3-06.Lists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d3_06.Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 2, 4, 6, 8, 10, 12, 14, 16 };
+            
+            if (list.Contains(4) && list.Contains(8) && list.Contains(12) && list.Contains(16))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
