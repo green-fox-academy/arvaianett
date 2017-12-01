33061b8737302617a7420ed865e7039361e86fa6
arvaianett <arvaianett@gmail.com>
2017-12-01 11:59:35 +0100

    feat(w2-day-05): add d3-01.Lists

---
+++ week-02/day-05/d3-01.Lists/d3-01.Lists/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d3_01.Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+
+            planetList.Insert(5, "Saturn");
+
+            Console.WriteLine(planetList);
+
+            foreach (string p in planetList)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
