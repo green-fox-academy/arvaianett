f919661f54c05a2650753593c2ad943c43572df6
arvaianett <arvaianett@gmail.com>
2017-12-01 14:31:09 +0100

    feat(w2-day-05): add d3 04. Lists

---
+++ week-02/day-05/d3-04.Lists/d3-04.Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d3_04.Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            list[1] = "Croissant";
+            list[3] = "Ice cream";
+
+            foreach (var l in list)
+            {
+                Console.WriteLine(l);
+            }
+            Console.ReadLine();
+        }
+    }
+}
