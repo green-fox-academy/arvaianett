e06b336a0ebcd272905172c23f7c3745002abd5d
arvaianett <arvaianett@gmail.com>
2017-12-02 14:29:33 +0100

    feat(w2-weekend): add d3 05.Lists

---
+++ week-02/weekend/d3-05.Lists/d3-05.Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d3_05.Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hoorray");
+            }
+            else
+            {
+                Console.WriteLine("Noooooo");
+            }
+            Console.ReadLine();
+
+            if (List.IndexOf(7) < 0)
+            {
+                Console.WriteLine("Noooooo");
+            }
+            else
+            {
+                Console.WriteLine("Hoorray");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
