ef936668b8da12dca125d66a1ce19388bfd5c245
arvaianett <arvaianett@gmail.com>
2017-12-02 15:08:36 +0100

    feat(w2-weekend): add d3 07.Lists

---
+++ week-02/weekend/d3.07.Lists/d3.07.Lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d3._07.Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            string fix01 = list[2];
+            string fix02 = list[5];
+
+            list[2] = fix02;
+            list[5] = fix01;
+
+            foreach (var a in list)
+            {
+                Console.Write(a);
+            }
+
+            Console.ReadLine();
+
+            for (int i = 0; i < 8; i++)
+            {
+                list[i] = list[i] + " ";
+            }
+
+            foreach (var b in list)
+            {
+                Console.Write(b);
+            }
+            Console.ReadLine();            
+        }
+    }
+}
