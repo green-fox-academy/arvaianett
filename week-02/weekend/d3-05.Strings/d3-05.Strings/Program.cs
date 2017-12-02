c5e18ef999733bb62f73ed422606b11510caa1ca
arvaianett <arvaianett@gmail.com>
2017-12-02 17:51:32 +0100

    feat(w2-weekend): add function d3 05. Strings

---
+++ week-02/weekend/d3-05.Strings/d3-05.Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace d3_05.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+            Console.WriteLine(Reverse(reversed));
+            Console.ReadLine();
+        }
+
+        static string Reverse(string r)
+        {
+            r.Split();
+
+            Array.Reverse(r);
+
+            return r;
+        }
+    }
+}
