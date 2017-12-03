3229f13a05247d38480eb1bf2cb4e38c6e1b16d3
arvaianett <arvaianett@gmail.com>
2017-12-03 13:42:12 +0100

    feat(w2-weekend): add d5 01.Dictionary

---
+++ week-02/weekend/d5-01.Dictionary/d5-01.Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace d5_01.Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "";
+            int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
+
+            var map = new Dictionary<int, string>();
+
+            map.Add(7, "run around and desert you ");
+            map.Add(50, "tell a lie and hurt you ");
+            map.Add(49, "make you cry, ");
+            map.Add(2, "let you down ");
+            map.Add(12, "give you up, ");
+            map.Add(1, "Never gonna ");
+            map.Add(11, "\n");
+            map.Add(3, "say goodbye ");
+
+            string value = map[1] + map[12] + map[1] + map[2] + map[11] + map[1] + map[7] + map[11] + map[11] +
+                            map[1] + map[49] + map[1] + map[3] + map[11] + map[1] + map[50] + map[11];
+
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
