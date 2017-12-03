f4002a1ab0870fb37b65a340450c547af4b2d410
arvaianett <arvaianett@gmail.com>
2017-12-03 13:30:33 +0100

    feat(w2-weekend): add d3 04.Strings

---
+++ week-02/weekend/d3-04.Strings/d3-04.Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace d3_04.Strings
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My todo{0} - Buy milk{0} - Download games{0}     - Diablo", Environment.NewLine);
+
+            Console.ReadLine();
+        }
+    }
+}
