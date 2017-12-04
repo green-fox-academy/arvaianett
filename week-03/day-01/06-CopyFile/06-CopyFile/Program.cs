a6c73901fa7cc498d89fd0d0a0720dd685fc05e8
arvaianett <arvaianett@gmail.com>
2017-12-04 17:20:48 +0100

    feat(w3-day-01): add 06-CopyFile

---
+++ week-03/day-01/06-CopyFile/06-CopyFile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace _06_CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"D:\Homework\my-file.txt";
+            string path2 = @"D:\Homework\file2";
+
+            Console.WriteLine(Copy(path1, path2));
+
+            Console.ReadLine();
+        }
+
+        static bool Copy(string path1, string path2)
+        {
+            try
+            {
+                string[] copyFrom = File.ReadAllLines(path1);
+
+                File.WriteAllLines(path2, copyFrom);
+
+                string[] copyTo = File.ReadAllLines(path2);
+                
+                if (copyFrom.Length == copyTo.Length)
+                {                    
+                    Console.WriteLine("The copy was succesful");
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(0);
+
+                return false;
+            }            
+        }
+    }
+}
