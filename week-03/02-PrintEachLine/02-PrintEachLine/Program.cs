d8c325bfd9f63ea298f77bc4cd56e9f228bfcc5d
arvaianett <arvaianett@gmail.com>
2017-12-04 13:42:29 +0100

    feat(w3-day-01): add 02-PrintEachLine

---
+++ week-03/02-PrintEachLine/02-PrintEachLine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _02_PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(@"D:/Homework/my-file.txt");
+
+                string line = "";
+
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to read file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
