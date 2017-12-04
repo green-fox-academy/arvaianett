4ca81c0037d81c3a5a7025d02de5ffe27eb927f6
arvaianett <arvaianett@gmail.com>
2017-12-04 14:11:36 +0100

    feat(w3-day-01): add 03-CountLines

---
+++ week-03/03-CountLines/03-CountLines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _03_CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Filename: ");
+
+            string input = Console.ReadLine();
+
+            FileName(input);
+
+            Console.ReadLine();
+        }
+
+        static int FileName(string name)
+        {
+            int number = 0;
+
+            try
+            {
+                string[] array = File.ReadAllLines(name);
+
+                number = array.Length;
+
+                Console.WriteLine(number);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(0);
+            }
+            return number;
+        }
+    }
+}
