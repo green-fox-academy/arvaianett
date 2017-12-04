1f7aa99bafe9c5b0fc157d3f3d70be39ce93a9d8
arvaianett <arvaianett@gmail.com>
2017-12-04 16:04:06 +0100

    feat(w3-day-01): add 05-WriteMultipleLines

---
+++ week-03/day-01/05-WriteMultipleLines/05-WriteMultipleLines/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace _05_WriteMultipleLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Path: ");
+            string path = Console.ReadLine();
+
+            Console.WriteLine("Word: ");
+            string word = Console.ReadLine();
+
+            Console.WriteLine("Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Function(path, word, number));
+
+            Console.ReadLine();
+        }
+
+        static string[] Function(string path, string word, int number)
+        {
+            string[] array = new string[number];
+
+            try
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    array[i] = word;
+                }
+
+                File.WriteAllLines(path, array);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(0);
+            }
+            return array;
+        }
+    }
+}
