aff833228eba73dfce607f9eaa58edf9d58a6678
arvaianett <arvaianett@gmail.com>
2017-12-04 12:17:05 +0100

    feat(w3-day-01): add 01-DivideByZero

---
+++ week-03/01-DivideByZero/01-DivideByZero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01_DivideByZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number: ");
+
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(DivideTen(input));
+
+            Console.ReadLine();
+        }
+
+        static int DivideTen(int number)
+        {
+            int result = 0;
+
+            try
+            {
+                result = 10 / number;                             
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("fail");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return result;
+        }
+    }
+}
