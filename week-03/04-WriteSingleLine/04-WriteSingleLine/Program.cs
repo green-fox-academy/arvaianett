d58f853a9fd3093d65b5eef3a0d415aee5ea6e3e
arvaianett <arvaianett@gmail.com>
2017-12-04 14:45:07 +0100

    feat(w3-day-01): add 04-WriteSingleLine

---
+++ week-03/04-WriteSingleLine/04-WriteSingleLine/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace _04_WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\Homework\my-file.txt");
+
+            string line = "";
+
+            while (line != null)
+            {
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+
+            sr.Close();
+
+            try
+            {
+                StreamWriter sw = new StreamWriter(@"D:\Homework\my-file.txt", true);
+
+                string input = "Anett";
+
+                sw.WriteLine(input);
+
+                sw.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
