3d5347b7e8e55db4017b97effd5be932a55cd527
arvaianett <arvaianett@gmail.com>
2017-12-06 13:32:27 +0100

    feat(w3-day-03): add 04. To the center

---
+++ week-03/day-03/04-ToTheCenter/04-ToTheCenter/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _04_ToTheCenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            Random rnd = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {              
+                int pointX = rnd.Next(500);
+                
+                int pointY = rnd.Next(400);
+                Lines(foxDraw, pointX, pointY);
+            }
+        }
+        public static void Lines(FoxDraw foxDraw, int pointX, int pointY)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            var startPoint = new Point(pointX, pointY);
+
+            var endPoint = new Point(200, 250);
+            foxDraw.DrawLine(startPoint, endPoint);
+
+        }
+    }
+}
