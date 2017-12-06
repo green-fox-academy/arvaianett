998d13b20ec6612bcb922f6e7116ac7c8ee48dce
arvaianett <arvaianett@gmail.com>
2017-12-06 17:09:17 +0100

    feat:rainbow

---
+++ week-03/day-03/10-RainbowBoxes/10-RainbowBoxes/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _10_RainbowBoxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            Random rnd = new Random();
+
+            int size = 500;
+
+            for (int i = 0; i < 115; i++)
+            {
+                Color randomColor = Color.FromRgb((byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256));
+
+                size = size - 4;
+
+                Square(foxDraw, size, randomColor);
+            }
+        }
+        public static void Square(FoxDraw foxDraw, int size, Color randomColor)
+        {
+            int x = (500 - size) / 2;
+            int y = (500 - size) / 2;
+            foxDraw.FillColor(randomColor);
+            foxDraw.DrawRectangle(x, y, size, size);
+        }
+    }
+}
