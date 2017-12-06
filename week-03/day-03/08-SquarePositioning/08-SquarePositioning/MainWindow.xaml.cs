2d85ae4be7ad9b3df1fa728f4fdfee22f7502673
arvaianett <arvaianett@gmail.com>
2017-12-06 14:18:43 +0100

    feat(w3-day-03): add 08. Square positioning

---
+++ week-03/day-03/08-SquarePositioning/08-SquarePositioning/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _08_SquarePositioning
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+    
+            Random rnd = new Random();
+
+            int pointX = 0;
+            int pointY = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                pointX = rnd.Next(500);
+                pointY = rnd.Next(400);
+                Square(foxDraw, pointX, pointY);           
+            }
+        }
+        public static void Square(FoxDraw foxDraw, int pointX, int pointY)
+        {
+            foxDraw.DrawRectangle(pointX, pointY, 50, 50);
+        }
+    }
+}
