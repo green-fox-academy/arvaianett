5fec76d7cc889bde90da35c2df92dbb88144f0f0
arvaianett <arvaianett@gmail.com>
2017-12-06 15:06:34 +0100

    feat(w3-day-03): add 09. Centered boxes

---
+++ week-03/day-03/09-CenteredBoxes/09-CenteredBoxes/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _09_CenteredBoxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var foxDraw = new FoxDraw(canvas);
+
+            double size = 0;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                size = rnd.Next(300);
+                Square(foxDraw, size);
+            }
+
+        }
+        public static void Square(FoxDraw foxDraw, double size)
+        {
+            double x = (500 - size) / 2;
+            double y = (300 - size) / 2;
+
+            foxDraw.DrawRectangle(x, y, size, size);
+        }
+    }
+}
