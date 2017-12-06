564a315023ef06ffdb78efadc16fac5660e02bab
arvaianett <arvaianett@gmail.com>
2017-12-06 13:55:45 +0100

    feat(w3-day-03): add 06. Square in the center

---
+++ week-03/day-03/06-SquareInTheCenter/06-SquareInTheCenter/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _06_SquareInTheCenter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawRectangle(foxDraw);
+        }
+        public static void DrawRectangle(FoxDraw foxDraw)
+        {
+            foxDraw.DrawRectangle(222.5, 195, 10, 10);
+        }
+    }
+}
