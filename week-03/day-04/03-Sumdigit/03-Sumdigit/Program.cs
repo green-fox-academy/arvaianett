032e9eca090394e8ae973b35bfd993b85862174d
arvaianett <arvaianett@gmail.com>
2017-12-07 16:37:32 +0100

    feat(w3-day-04): add 04. Bunnies

---
+++ week-03/day-04/03-Sumdigit/03-Sumdigit/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Sumdigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 136;
+
+            Console.WriteLine(Sum(number));
+            Console.ReadLine();
+        }
+        static int Sum(int number)
+        {
+            if (number < 10)
+            {
+                return number;
+            }
+            else
+            {                              
+                return number % 10 + Sum(number / 10);               
+            }
+        }
+    }
+}
---
+++ week-03/day-04/04.Bunnies/04.Bunnies/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunnies = 4;
+
+            Console.WriteLine(Ears(bunnies));
+
+            Console.ReadLine();
+        }
+        static int Ears(int bunnies)
+        {
+            if (bunnies == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Ears(--bunnies) + 2;
+            }
+        }
+    }
+}
