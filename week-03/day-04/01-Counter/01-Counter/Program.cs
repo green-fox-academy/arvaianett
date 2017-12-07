24abd137552145913209bfada6522acfde4b5a4c
arvaianett <arvaianett@gmail.com>
2017-12-07 12:13:11 +0100

    feat(w3-day-04): add 01. Counter

---
+++ week-03/day-04/01-Counter/01-Counter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Count(number);
+            Console.ReadLine();
+        }
+        public static int Count(int number)
+        {
+            if(number == 0)
+            {
+                Console.WriteLine(number);
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine(number);
+                return Count(--number);
+            }
+        }
+    }
+}
