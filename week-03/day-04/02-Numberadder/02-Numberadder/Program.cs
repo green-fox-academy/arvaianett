6df7eaf5e350148d1de1e6ed37221a05bb4d5667
arvaianett <arvaianett@gmail.com>
2017-12-07 14:09:55 +0100

    feat(w3-day-04): add 02. Numberadder

---
+++ week-03/day-04/02-Numberadder/02-Numberadder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Numberadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int counter = 0;
+
+            Console.WriteLine(Add(number, counter));
+            
+            Console.ReadLine();
+        }
+        static int Add(int number, int counter1)
+        {
+            if (number == 0)
+            {
+                return counter1;
+            }
+            else
+            {
+                return Add(number - 1, counter1 += number);
+            }
+        }
+    }
+}
