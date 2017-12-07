2741351f62db8b9a924fe839c7d807e75215feca
arvaianett <arvaianett@gmail.com>
2017-12-07 17:26:57 +0100

    feat(w3-day-04): add 06. Bunnies again

---
+++ week-03/day-04/06-BunniesAgain/06-BunniesAgain/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_BunniesAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bunnies = 5;
+            int counter = 0;
+
+            Console.WriteLine(Ears(bunnies, counter));
+            
+            Console.ReadLine();
+        }
+        static int Ears(int bunnies, int counter)
+        {
+            if (bunnies == 0)
+            {              
+                return counter;
+            }
+            else
+            {
+                if(bunnies % 2 == 0)
+                {
+                    Ears(--bunnies, counter += 3);
+                }
+                else if(bunnies % 2 != 0)
+                {
+                    Ears(--bunnies, counter += 2);
+                }
+                return Ears(bunnies, counter);
+            }
+        }
+    }
+}
