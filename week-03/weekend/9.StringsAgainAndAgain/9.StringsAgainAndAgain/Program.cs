6e0c332c03f073e457c00d3d94d6e97dd8593e02
arvaianett <arvaianett@gmail.com>
2017-12-10 16:06:22 +0100

    feat(w3-day-04): add 9. Strings3

---
+++ week-03/weekend/9.StringsAgainAndAgain/9.StringsAgainAndAgain/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.StringsAgainAndAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "salalala";
+
+            Console.WriteLine(Separate(input));
+            Console.ReadLine();
+        }
+        static string Separate(string input)
+        {
+            if (input.LastIndexOf("*") == input.Length - 2)
+            {
+                return input;
+            }
+            else
+            {               
+                input = input.Insert(input.LastIndexOf("*") + 2, "*" );
+                return Separate(input);
+            }
+        }
+    }
+}
