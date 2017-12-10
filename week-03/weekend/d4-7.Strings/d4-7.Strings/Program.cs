7f2d302998ec5a8f3d71868826f57b8416e9bbec
arvaianett <arvaianett@gmail.com>
2017-12-10 14:17:05 +0100

    feat(w3-day-04): add 7. Strings

---
+++ week-03/weekend/d4-7.Strings/d4-7.Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d4_7.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = "xxXXxx";
+
+            Console.WriteLine(Change(letters));
+            Console.ReadLine();
+        }
+        static string Change(string letters)
+        {
+            letters = letters.Replace("x", "y");
+            return letters;
+        }
+    }
+}
