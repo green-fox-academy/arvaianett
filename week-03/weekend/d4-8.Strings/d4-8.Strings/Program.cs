93ab9c4835e621cc022b58f7cf4d2757b22cccd7
arvaianett <arvaianett@gmail.com>
2017-12-10 14:57:46 +0100

    feat(w3-day-04): add 8. Strings again

---
+++ week-03/weekend/d4-8.Strings/d4-8.Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d4_8.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = "xxXXYY";
+
+            Console.WriteLine(Remove(letters));
+            Console.ReadLine();
+        }
+        static string Remove(string letters)
+        {
+            letters = letters.Replace("x", string.Empty);
+            return letters;
+        }
+    }
+}
