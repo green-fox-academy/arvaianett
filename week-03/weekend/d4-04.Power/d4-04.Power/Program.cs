8683d445d25d3fb8644e05d7a29f69d876085144
arvaianett <arvaianett@gmail.com>
2017-12-10 14:03:34 +0100

    feat(w3-day-04): add 04. Power

---
+++ week-03/weekend/d4-04.Power/d4-04.Power/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d4_04.Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 3;
+            int p = 3;
+                        
+            Console.WriteLine(Power(number, p));
+            Console.ReadLine();
+        }
+        static int Power(int number, int p)
+        {
+            if (p == 1)
+            {
+                return number;
+            }
+            else
+            {           
+                return number * Power(number, --p);               
+            }
+        }
+    }
+}
