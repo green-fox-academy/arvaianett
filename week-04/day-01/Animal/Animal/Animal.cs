5cae598ee85f47bd426d224cd3a458184bed19a8
arvaianett <arvaianett@gmail.com>
2017-12-11 16:33:06 +0100

    feat(w3-day-01): add Animal

---
+++ week-04/day-01/Animal/Animal/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public int hunger;
+        public int thirst;
+
+        public void Eat()
+        {
+            hunger -= 1;
+        }
+        public void Drink()
+        {
+            thirst -= 1;
+        }
+        public void Play()
+        {
+            hunger += 1;
+            thirst += 1;
+        }
+    }
+}
---
+++ week-04/day-01/Animal/Animal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+
+            animal1.hunger = 50;
+            animal1.hunger = 50;
+
+            animal1.Eat();
+
+            Console.WriteLine(animal1.hunger);
+            Console.ReadLine();
+        }
+    }
+}
