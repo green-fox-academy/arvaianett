ccab2ce029e016fa3b08529219da8466a64af9cc
arvaianett <arvaianett@gmail.com>
2017-12-11 16:50:59 +0100

    feat(w4-day-01): add Sharpie

---
+++ week-04/day-01/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sharpie sharpie1 = new Sharpie();
+
+            sharpie1.color = "blue";
+            sharpie1.width = 20;
+
+            sharpie1.Use();
+
+            Console.WriteLine(sharpie1.InkAmount);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ week-04/day-01/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class Sharpie
+    {
+        public string color;
+        public float width;
+        public float InkAmount;
+
+        public Sharpie()
+        {
+            InkAmount = 100;
+        }
+
+        public void Use()
+        {
+            InkAmount -= 1;
+        }
+    }
+}
