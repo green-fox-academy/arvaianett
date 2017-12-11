0be768257722b67cc388538fa393a2344d390af6
arvaianett <arvaianett@gmail.com>
2017-12-11 15:50:46 +0100

    feat(w3-day-01): add Dice

---
+++ week-04/day-01/Dice/Dice/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Dice
+    {
+        public int D6 { get; set; }
+
+        public static Random random = new Random();
+
+        public void Roll()
+        {           
+            D6 = random.Next(1, 7);
+        }
+        public int GetCurrent(Dice current)
+        {
+            return current.D6;
+        }
+        public void Reroll()
+        {           
+            D6 = random.Next(1, 7);
+        }
+        public bool Six(Dice dice)
+        {
+            if (dice.D6 == 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ week-04/day-01/Dice/Dice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice dice1 = new Dice();
+            Dice dice2 = new Dice();
+
+            dice1.Roll();
+            Console.WriteLine(dice1.D6);
+
+            Console.WriteLine(dice1.GetCurrent(dice1));
+
+            dice1.Reroll();
+            Console.WriteLine(dice1.D6);
+           
+            Console.WriteLine(dice1.Six(dice1));
+
+            Console.ReadLine();           
+        }
+    }
+}
