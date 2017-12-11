d46bb4fbf2805a6867d88718e85a1f3e7625b528
arvaianett <arvaianett@gmail.com>
2017-12-11 11:56:54 +0100

    feat(w4-day-01): add Post-it

---
+++ week-04/day-01/Post-it/Post-it/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_it
+{
+    class Program
+    {
+        public struct PostIt
+        {
+            public string BackgroundColor;
+            public string Text;
+            public string TextColor;
+        }
+        static void Main(string[] args)
+        {
+            PostIt postIt1;
+
+            postIt1.BackgroundColor = "";
+            postIt1.Text = "";
+            postIt1.TextColor = "";
+        }
+    }
+}
