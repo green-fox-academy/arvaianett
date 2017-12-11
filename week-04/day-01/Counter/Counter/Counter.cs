cbf07c132673eaf3282a406f6c3065e22afc7d36
arvaianett <arvaianett@gmail.com>
2017-12-11 17:27:29 +0100

    feat(w4-day-01): add Counter

---
+++ week-04/day-01/Counter/Counter/Counter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Counter
+    {
+        public int C { get; set; }       
+
+        public Counter()
+        {
+            C = 0;
+        }
+
+        public void Add()
+        {
+            C += 1;
+        }
+
+        public void Get()
+        {
+            C.ToString() ;
+        }
+
+        public void Reset()
+        {
+            C = 0;
+        }
+    }
+}
---
+++ week-04/day-01/Counter/Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = new Counter();
+            
+            counter1.Add();
+            Console.WriteLine(counter1.C);
+
+            counter1.Get();
+            Console.WriteLine(counter1.C);
+
+            counter1.Reset();
+            Console.WriteLine(counter1.C);
+
+            Console.ReadLine();
+        }
+    }
+}
