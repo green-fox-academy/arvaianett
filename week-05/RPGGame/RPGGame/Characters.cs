7b65ea69bc73e7be7cc540db4e132ecdf16814ca
arvaianett <arvaianett@gmail.com>
2017-12-18 17:35:10 +0100

    feat(RPG): add Floor and Hero's picture

---
+++ week-05/RPGGame/RPGGame/Characters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RPGGame
+{
+    public class Characters
+    {
+        private Canvas canvas;
+
+        public Characters(Canvas canvas)
+        {
+            this.canvas = canvas;
+        }
+
+        public void AddHero()
+        {
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.AddImage("./Assets/hero-down.png", 0, 0);
+        }
+    }
+}
---
+++ week-05/RPGGame/RPGGame/Floor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace RPGGame
+{
+    internal class Floor
+    {
+        Random randomNumber = new Random();
+
+        private const int FloorMap = 10;
+        private const int TileSize = 50;
+        private const int AmountOfWallTile = 43;
+        private const int TableSize = 500;
+
+        private Canvas canvas;
+
+        public Floor(Canvas canvas)
+        {
+            this.canvas = canvas;
+        }
+
+        public void AddFloor()
+        {
+            var foxDraw = new FoxDraw(canvas);
+
+            int coordinateX = 0;
+            int coordinateY = 0;
+
+            bool[,] FloorMap = new bool[10, 10] { { true, true, false, false, true, false, false, true, false, true},
+                                                  { true, true, false, false, true, false, true, true, true, true},
+                                                  { true, true, true, true, true, true, true, true, false, true},
+                                                  { false, true, false, false, true, false, true, true, false, true},
+                                                  { false, true, true, false, true, false, true, true, false, true},
+                                                  { true, true, false, false, true, false, true, true, true, true},
+                                                  { false, true, false, false, true, false, true, false, false, true},
+                                                  { true, true, true, true, true, false, true, false, false, true},
+                                                  { true, true, false, false, true, false, true, true, true, true},
+                                                  { false, false, false, false, true, false, true, true, false, false}};
+
+            for (int i = 0; i < 10; i++)
+            {               
+                for (int j = 0; j < 10; j++)
+                {
+                    if (FloorMap[i, j] == true)
+                    {
+                        foxDraw.AddImage("./Assets/floor.png", coordinateX, coordinateY);
+                    }
+                    else
+                    {
+                        foxDraw.AddImage("./Assets/wall.png", coordinateX, coordinateY);
+                    }
+                    coordinateX += TileSize;
+                }
+                coordinateX = 0;
+                coordinateY += TileSize;
+            }
+        }
+    }
+}
