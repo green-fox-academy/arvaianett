c38c13ee195dcf5a41038eb538b1d05bb4eaf6f6
arvaianett <arvaianett@gmail.com>
2017-12-21 21:24:56 +0100

    feat(week-05): add Favourite Animals (Pallida exam)

---
+++ week-05/FavouriteAnimals/FavouriteAnimals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FavouriteAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Save animals = new Save();
+
+            if (args.Length == 0)
+            {
+                string path = @"D:\greenfox\arvaianett\week-05\FavouriteAnimals\animals.txt";
+
+                string readAnimals = File.ReadAllText(path);
+
+                Console.WriteLine(readAnimals);
+            }
+
+            else if (args.Length != 0)
+            {
+                animals.SaveToTextFile(args);
+            }
+        }
+    }
+}
---
+++ week-05/FavouriteAnimals/FavouriteAnimals/Save.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace FavouriteAnimals
+{
+    class Save
+    {
+        public void SaveToTextFile(string[] addAnimal)
+        {
+            string path = @"D:\greenfox\arvaianett\week-05\FavouriteAnimals\animals.txt";
+
+            string[] animalsArray = File.ReadAllLines(path);
+        
+            Dictionary<string, int> animalDictionary = new Dictionary<string, int>();
+
+            for (int i = 0; i < animalsArray.Length; i++)
+            {                              
+                if (!animalDictionary.ContainsKey(animalsArray[i]))
+                {
+                    animalDictionary.Add(animalsArray[i], i);
+                }
+                else
+                {
+                    int count = 0;
+                    animalDictionary.TryGetValue(animalsArray[i], out count);
+                    animalDictionary.Remove(animalsArray[i]);
+                    animalDictionary.Add(animalsArray[i], count + 1);
+                }
+            }
+            for (int i = 0; i < addAnimal.Length; i++)
+            {
+                if (!animalDictionary.ContainsKey(addAnimal[i]))
+                {
+                    animalDictionary.Add(addAnimal[i], i);
+
+                    string animal = addAnimal[i].ToString();
+
+                    AppendAnimals(animal);
+                }
+                else
+                {
+                    int count = 0;
+                    animalDictionary.TryGetValue(addAnimal[i], out count);
+                    animalDictionary.Remove(addAnimal[i]);
+                    animalDictionary.Add(addAnimal[i], count + 1);
+                }
+            }
+        }
+
+        public void AppendAnimals(string animal)
+        {
+            string path = @"D:\greenfox\arvaianett\week-05\FavouriteAnimals\animals.txt";
+
+            StreamWriter appendAnimalToFile = new StreamWriter(path,true);
+
+            appendAnimalToFile.WriteLine(animal);
+
+            appendAnimalToFile.Close();
+        }
+    }
+}
