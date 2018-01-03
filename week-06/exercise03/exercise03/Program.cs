f53634206c377a605338032d28400280e87ac913
arvaianett <arvaianett@gmail.com>
2018-01-03 14:41:58 +0100

    feat(w6-day-02): add Linq exercise 03

---
+++ week-06/exercise03/exercise03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> squaredPosList = n.Where(x => x > 0)
+                .Select(x => x * x)
+                .ToList();
+
+            foreach (int numbers in squaredPosList)
+            {
+                Console.WriteLine(numbers);
+            }
+
+            IEnumerable<int> squaredPostListQuery =
+                from number in n
+                where number > 0
+                select number * number;
+
+            foreach (int number in squaredPostListQuery)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
