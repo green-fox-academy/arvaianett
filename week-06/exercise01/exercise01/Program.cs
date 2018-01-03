62001dc108629bb17f0df977d8eef54e2fb8f3a8
arvaianett <arvaianett@gmail.com>
2018-01-03 13:42:56 +0100

    feat(w6-day-02): add Linq exercise 01

---
+++ week-06/exercise01/exercise01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> evenNumberMethod = n.Where(x => x % 2 == 0).ToList();
+
+            foreach (int number in evenNumberMethod)
+            {
+                Console.Write(number);
+            }
+
+            IEnumerable<int> evenNumberQuery =
+                from number in n
+                where number % 2 == 0
+                select number;
+
+            foreach (int number in evenNumberQuery)
+            {
+                Console.Write(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
