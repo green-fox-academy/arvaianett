65296c49d7f17139d9ae80e879fd19b06ed17ece
arvaianett <arvaianett@gmail.com>
2018-01-03 16:15:06 +0100

    feat(w6-day-02): add Linq exercise 04

---
+++ week-06/day-02/exercise04/exercise04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+
+            IEnumerable<double> list = n.Select(x => x * x)
+                .Where(x => x > 20)
+                .Select(x => Math.Sqrt(x))
+                .ToList();
+
+            foreach (int number in list)
+            {
+                Console.WriteLine(number);
+            }
+
+            IEnumerable<double> listQ =
+                from number in n
+                let anotherNumber = number * number
+                where anotherNumber > 20
+                select Math.Sqrt(anotherNumber);
+
+            foreach (double number in listQ)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
