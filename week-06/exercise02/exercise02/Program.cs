8c9ac89cd6f0f55a6f661986713bc71d77351beb
arvaianett <arvaianett@gmail.com>
2018-01-03 14:03:41 +0100

    feat(w6-day-02): add Linq exercise 02

---
+++ week-06/exercise02/exercise02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            int averageNumber = Convert.ToInt16(n.Average());
+
+            Console.WriteLine(averageNumber);
+
+            int averageWithQuery =
+                Convert.ToInt16((from number in n
+                select number).Average());
+
+            Console.WriteLine(averageWithQuery);
+
+            Console.ReadLine();
+        }
+    }
+}
