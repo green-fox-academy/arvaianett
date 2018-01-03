44ce58bd0701b4fe10e3bf3e1c21892b1e40b564
arvaianett <arvaianett@gmail.com>
2018-01-03 16:53:52 +0100

    feat(w6-day-02): add Linq exercise 06

---
+++ week-06/day-02/exercise06/exercise06/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace exercise06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] inputArray = input.ToCharArray();
+
+            var frequencyMethod = inputArray.OrderBy(x => x)
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var item in frequencyMethod)
+            {
+                Console.WriteLine(item);
+            }
+
+            var frequencyQuery =
+                from i in input
+                orderby i
+                group i by i into b
+                select b;
+            var dictionary = frequencyQuery.ToDictionary(y => y.Key, y => y.Count());
+
+            foreach (var item in frequencyQuery)
+            {
+                Console.WriteLine(item.Key + " " + dictionary[item.Key]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
