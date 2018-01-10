35cff9ddb3ffd68142d10542098b4289a54e725a
arvaianett <arvaianett@gmail.com>
2018-01-10 13:20:38 +0100

    feat(day-03): add Simba Part I

---
+++ week-07/day-03/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankOfSimba.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("simba")]
+        public IActionResult Index(BankAccount bankAccount)
+        {
+            bankAccount.Name = "Simba";
+            bankAccount.Balance = 2000;
+            bankAccount.AnimalType = "Animal.Lion";
+
+            return View(bankAccount);
+        }
+    }
+}
