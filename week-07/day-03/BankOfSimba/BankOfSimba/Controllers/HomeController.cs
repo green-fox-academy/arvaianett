d8c5fab1398673cd5a930286758ea72ee6bb0d31
arvaianett <arvaianett@gmail.com>
2018-01-10 14:34:36 +0100

    feat(day-03): add Simba Part III

--- week-07/day-03/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
+++ week-07/day-03/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BankOfSimba.Models;
+using BankOfSimba.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +13,7 @@ namespace BankOfSimba.Controllers
     [Route("")]
     public class HomeController : Controller
     {
+        public static BankAccountViewModel BankAccountViewModel = new BankAccountViewModel();
         // GET: /<controller>/
         [Route("simba")]
         public IActionResult Index(BankAccount bankAccount)
@@ -22,6 +24,12 @@ namespace BankOfSimba.Controllers
             bankAccount.AnimalType = "Animal.Lion";
 
             return View(bankAccount);
+        }
+
+        [Route("lionking")]
+        public IActionResult Characters()
+        {
+            return View(BankAccountViewModel);
         }
     }
 }
---
+++ week-07/day-03/BankOfSimba/BankOfSimba/ViewModels/BankAccountViewModel.cs
@@ -0,0 +1,53 @@
+﻿using BankOfSimba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.ViewModels
+{
+    public class BankAccountViewModel
+    {
+        public List<BankAccount> BankAccountList = new List<BankAccount>();
+
+        public BankAccountViewModel()
+        {
+            InitAccountList();
+        }
+
+        public void InitAccountList()
+        {
+            BankAccountList.Add(new BankAccount()
+            {
+                Name = "Simba",
+                Balance = 2000.00,
+                Currency = "Zebra",
+                AnimalType = "Lion"
+            });
+
+            BankAccountList.Add(new BankAccount()
+            {
+                Name = "Musafa",
+                Balance = 1000.00,
+                Currency = "Zebra",
+                AnimalType = "Lion"
+            });
+
+            BankAccountList.Add(new BankAccount()
+            {
+                Name = "Zazu",
+                Balance = 500.50,
+                Currency = "Zebra",
+                AnimalType = "Lion"
+            });
+
+            BankAccountList.Add(new BankAccount()
+            {
+                Name = "Nala",
+                Balance = 900.00,
+                Currency = "Zebra",
+                AnimalType = "Lion"
+            });
+        }
+    }
+}
