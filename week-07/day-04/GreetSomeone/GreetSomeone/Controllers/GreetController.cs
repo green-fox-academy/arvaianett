0c68819c8aca2a210f31288d960f03ecc6002432
arvaianett <arvaianett@gmail.com>
2018-01-11 14:22:22 +0100

    feat(day-04): add Greet Someone

---
+++ week-07/day-04/GreetSomeone/GreetSomeone/Controllers/GreetController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetSomeone.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetSomeone.Controllers
+{
+    public class GreetController : Controller
+    {
+        public GreetModel greetModel;
+
+        public GreetController(GreetModel greetModel)
+        {
+            this.greetModel = greetModel;
+        }
+        // GET: /<controller>/
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(greetModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult PostMethod(string name)
+        {
+            greetModel.Name = name;
+            return RedirectToAction("Greet");
+        }
+
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return View(greetModel);
+        }
+    }
+}
