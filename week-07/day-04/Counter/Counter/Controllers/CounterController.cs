48c6aab9064af3d8531f0fda429a5b35b475d31d
arvaianett <arvaianett@gmail.com>
2018-01-11 12:15:10 +0100

    feat(day-04): add Counter

---
+++ week-07/day-04/Counter/Counter/Controllers/CounterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Counter.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Counter.Controllers
+{
+    public class CounterController : Controller
+    {
+        public CounterModel counterView;
+
+        public CounterController(CounterModel counterView)
+        {
+            this.counterView = counterView;
+        }
+        // GET: /<controller>/
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(counterView);
+        }
+
+        [HttpPost("")]
+        public IActionResult PostMethod()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
