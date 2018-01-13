027b7bb7af93e656fbb185ab69618676a7a2a843
arvaianett <arvaianett@gmail.com>
2018-01-13 10:16:15 +0100

    feat(day-04): add Anagram

---
+++ week-07/day-04/Anagram/Anagram/Controllers/AnagramController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Anagram.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Anagram.Controllers
+{
+    [Route("")]
+    public class AnagramController : Controller
+    {
+        public AnagramModel anagramModel;
+
+        public AnagramController(AnagramModel anagramModel)
+        {
+            this.anagramModel = anagramModel;
+        }
+
+        // GET: /<controller>/
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(anagramModel);
+        }
+
+        [HttpPost("add")]
+        public IActionResult PostMethod(string firstWord, string secondWord)
+        {
+            anagramModel.FirstWord = firstWord;
+            anagramModel.SecondWord = secondWord;
+
+            return RedirectToAction("result");
+        }
+
+        [HttpGet("result")]
+        public IActionResult Result()
+        {
+            return View(anagramModel);
+        }
+    }
+}
---
+++ week-07/day-04/Anagram/Anagram/Models/AnagramModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagram.Models
+{
+    public class AnagramModel
+    {
+        public string FirstWord { get; set; }
+        public string SecondWord { get; set; }
+    }
+}
