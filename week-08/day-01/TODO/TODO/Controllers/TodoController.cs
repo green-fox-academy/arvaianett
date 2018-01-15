1536bf76d57dbddc61c0d8fc7793664a26202368
arvaianett <arvaianett@gmail.com>
2018-01-15 13:30:10 +0100

    feat(day-01): add Todo Controller

---
+++ week-08/day-01/TODO/TODO/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TODO.Controllers
+{
+    [Route("/todo")]
+    public class TodoController : Controller
+    {
+        // GET: /<controller>/
+        [Route("/")]
+        [Route("/list")]
+        public IActionResult List()
+        {
+            return Content("This is my first todo");
+        }
+    }
+}
