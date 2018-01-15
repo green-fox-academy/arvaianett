1817c72738c027a1f9200351367d157d795fd0da
arvaianett <arvaianett@gmail.com>
2018-01-15 13:39:41 +0100

    feat(day-01): add folders to Todo exercise

---
+++ week-08/day-01/TODO/TODO/Entities/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TODO.Models;
+
+namespace TODO.Context
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
