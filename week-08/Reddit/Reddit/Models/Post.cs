5e307866489959358b7634b72b61f8f03383ee72
arvaianett <arvaianett@gmail.com>
2018-01-17 21:34:37 +0100

    feat(Reddit): add key

--- week-08/Reddit/Reddit/Models/Post.cs
+++ week-08/Reddit/Reddit/Models/Post.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Reddit.Models
 {
     public class Post
     {
+        [Key]
         public long PostId { get; set; }
         public int Score { get; set; }
         public string Content { get; set; }
--- week-08/Reddit/Reddit/Models/User.cs
+++ week-08/Reddit/Reddit/Models/User.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Reddit.Models
 {
     public class User
     {
+        [Key]
         public long UserId { get; set; }
         public string Username { get; set; }
 
