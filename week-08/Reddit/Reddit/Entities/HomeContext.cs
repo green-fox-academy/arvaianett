062d74646b08f3071e2b14c4e42fd183f9e2e4ee
arvaianett <arvaianett@gmail.com>
2018-01-17 21:36:37 +0100

    feat(Reddit): add HomeContext

---
+++ week-08/Reddit/Reddit/Entities/HomeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reddit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Entities
+{
+    public class HomeContext : DbContext
+    {
+        public HomeContext(DbContextOptions<HomeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
