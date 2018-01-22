259ddc0e1e8406dbeb3dc53df8a32325dcbad08e
arvaianett <arvaianett@gmail.com>
2018-01-22 12:24:21 +0100

    feat(day-01): add Doubling

--- week-09/day-01/Frontend/Frontend/Controllers/HomeController.cs
+++ week-09/day-01/Frontend/Frontend/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Frontend.Controllers
 {
+    [Route("api")]
     public class HomeController : Controller
     {
         // GET api/values
@@ -13,6 +14,28 @@ namespace Frontend.Controllers
         public IActionResult Index()
         {
             return File("index.html", "text/html");
+        }
+
+        [HttpGet("/doubling")]
+        public IActionResult Index(int? input)
+        {
+            if (input == null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            else
+            {
+                return Json(new { received = input, result = input * 2 });
+            }
+            //try
+            //{
+            //    int doubled = input * 2;
+            //    return Json(new { result = doubled });
+            //}
+            //catch (Exception)
+            //{
+            //    return Json(new { error = "Please provide an input!" });
+            //}
         }
     }
 }
---
+++ week-09/day-01/Frontend/Frontend/Models/Doubling.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Models
+{
+    public class Doubling
+    {
+        public int Received { get; set; }
+        public int Result { get; set; }
+        public string Error { get; set; }
+    }
+}
