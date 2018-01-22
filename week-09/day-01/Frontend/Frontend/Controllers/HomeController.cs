e302e1d4c40d68c528eea059932385cff41904ed
arvaianett <arvaianett@gmail.com>
2018-01-22 11:12:08 +0100

    feat(day-01): add Frontend

---
+++ week-09/day-01/Frontend/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET api/values
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+    }
+}
