edb1d36fa661586e0b4eb92099e9b7518e0cdd1a
arvaianett <arvaianett@gmail.com>
2018-02-17 14:07:42 +0100

    add thumbnail

--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -2,41 +2,42 @@
 
 const listOfPictures = [
     {'id': 1,
-    'picture': '01.jpg',
+    'picture': 'pictures/01.jpg',
     'title': '1st.title',
     'text': '1st.text'},
     {'id': 2,
-    'picture': '02.jpg',
+    'picture': 'pictures/02.jpg',
     'title': '2nd.title',
     'text': '2nd.text'},
     {'id': 3,
-    'picture': '03.jpg',
+    'picture': 'pictures/03.jpg',
     'title': '3th.title',
     'text': '3th.text'},
     {'id': 4,
-    'picture': '04.jpg',
+    'picture': 'pictures/04.jpg',
     'title': '4th.title',
     'text': '4th.text'},
     {'id': 5,
-    'picture': '05.jpg',
+    'picture': 'pictures/05.jpg',
     'title': '5th.title',
     'text': '5th.text'},
     {'id': 6,
-    'picture': '06.jpg',
+    'picture': 'pictures/06.jpg',
     'title': '6th.title',
     'text': '6th.text'},
     {'id': 7,
-    'picture': '07.jpg',
+    'picture': 'pictures/07.jpg',
     'title': '7th.title',
     'text': '7th.text'},
     {'id': 8,
-    'picture': '08.jpg',
+    'picture': 'pictures/08.jpg',
     'title': '8th.title',
     'text': '8th.text'}
 ]
 
+
 let picture = document.getElementsByTagName('img');
-picture[0].src = "pictures/01.jpg";
+picture[0].src = listOfPictures[0].picture;
 
 let title = document.getElementsByTagName('h2');
 title[0].innerHTML = "first";
@@ -44,4 +45,21 @@ title[0].innerHTML = "first";
 let text = document.getElementsByTagName('p');
 text[0].innerHTML = "hello";
 
-picture[1].src = "pictures/02.jpg";
+//create thumbnail
+
+listOfPictures.forEach(function(images) {
+
+    let gridDiv = document.createElement('div');
+    gridDiv.className = "col-2";
+
+    let thumbPicture = document.createElement('img');
+    thumbPicture.className = "img-thumbnail";
+    thumbPicture.src = images.picture;
+
+    let getParent = document.getElementById("thumbnail-images");
+
+    getParent.appendChild(gridDiv);
+    gridDiv.appendChild(thumbPicture);
+})
+
+
